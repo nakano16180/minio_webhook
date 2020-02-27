25e218d4d0c6d7447395ad36d7a4c6b7c54143fb
nakano16180 <nakano16180@gmail.com>
2020-02-27 12:23:41 +0900

    update

--- app.js
+++ app.js
@@ -4,38 +4,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+var config = require('config');
+
+var mcConfig = config.get('config');
+if (mcConfig.endPoint === '<endpoint>') {
+    console.log('Please configure your endpoint in \"config/webhook.json\".');
+    process.exit(1);
+}
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname+'/'));
 
 const PORT = 3000;
 
-const axios = require('axios');
-const url = "http://d-lambda:9001/2015-03-31/functions/myfunction/invocations";
-
-async function main(req, res) {
-  try {
-    const result = await axios.post(url, req.body);
-    console.log(result);
-    console.log('###########################################');
-    return result;
-  } catch (error) {
-    console.log('catch error');
-    console.log('###########################################');
-    console.log(error);
-    console.log('###########################################');
-  }
-}
-
 
 app.post('/', (req, res) => {
-    console.log(req.body); //Minio serverから送られて来る情報の確認
-    const result = main(req, res);
-    //res.end(JSON.stringify({}));
+  console.log(req); //Minio serverから送られて来る情報の確認
+  console.log('-----------')
+  console.log(req.body); //Minio serverから送られて来る情報の確認
+  res.end(JSON.stringify({}));
 });
 
 
-app.listen(PORT);
-console.log(`listening on *:${PORT}`);
+var server = app.listen(PORT, () =>{
+  console.log(`listening on *:${PORT}`);
+});
+
 
 
 var process = require('process')
