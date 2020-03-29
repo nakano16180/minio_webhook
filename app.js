b80d1f2b2aca27680ea565f82be91c0f163e056e
nakano16180 <nakano16180@gmail.com>
2020-03-29 14:03:46 +0900

    update app.js

--- app.js
+++ app.js
@@ -17,12 +17,25 @@ app.use(express.static(__dirname+'/'));
 
 const port = process.env.PORT || 3000;
 
+function baseName(str){
+  var base = new String(str).substring(str.lastIndexOf('/') + 1); 
+   if(base.lastIndexOf(".") != -1)       
+       base = base.substring(0, base.lastIndexOf("."));
+  return base;
+}
 
 app.post('/', (req, res) => {
-  console.log(req); //Minio serverから送られて来る情報の確認
-  console.log('-----------')
-  console.log(req.body); //Minio serverから送られて来る情報の確認
-  res.end(JSON.stringify({}));
+  var bucketName = req.body.Records[0].s3.bucket.name;
+  console.log('==== bucket information =====');
+  console.log(bucketName);
+  console.log('=============================');
+
+  var uplodedFile = req.body.Records[0].s3.object.key;
+  console.log('==== uploaded file ====');
+  console.log(uplodedFile);
+  var filename = baseName(uplodedFile);
+  console.log(filename);
+  console.log('=============================');
 });
 
 
