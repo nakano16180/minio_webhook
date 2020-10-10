59ca21259cc7d0755475a413d7445fe348830b55
nakano16180 <nakano16180@gmail.com>
2020-10-10 18:30:24 +0900

    update set-bucket-notification

--- set-bucket-notification.js
+++ set-bucket-notification.js
@@ -1,48 +1,48 @@
-var Minio = require('minio')
+var Minio = require("minio");
 
-endpoint_url = "localhost"
-PORT = 9000
-ACCESS_KEY = "minioadmin"
-SECRET_KEY = "minioadmin"
+const endpoint_url = "localhost";
+const PORT = 9000;
+const ACCESS_KEY = "minioadmin";
+const SECRET_KEY = "minioadmin";
 
 var minioClient = new Minio.Client({
   endPoint: endpoint_url,
   port: PORT,
   useSSL: false,
   accessKey: ACCESS_KEY,
-  secretKey: SECRET_KEY
+  secretKey: SECRET_KEY,
 });
 
-
-minioClient.bucketExists('mybucket', function(err, exists) {
+minioClient.bucketExists("mybucket", function (err, exists) {
   if (err) {
-    return console.log(err)
+    return console.log(err);
   }
-  if (exists) {
-    //  Bucket Notification //////////////////////////////////////////
-    //  Create a new notification object
-    var bucketNotification = new Minio.NotificationConfig();
-
-    // Setup a new Queue configuration
-    var arn = Minio.buildARN('minio', 'sqs', '', 1, 'webhook')
-    var queue = new Minio.QueueConfig(arn)
-
-    queue.addFilterSuffix('.mp4')
-    queue.addEvent(Minio.ObjectCreatedAll)
-
-    // Add the queue to the overall notification object
-    bucketNotification.add(queue)
-
-    minioClient.setBucketNotification('mybucket', bucketNotification, function(err) {
-      if (err) return console.log(err)
-      console.log('Success')
-    })
-
-    return console.log('Bucket exists.')
-  }else{
-    minioClient.makeBucket('mybucket', 'us-east-1', function(err) {
-      if (err) return console.log('Error creating bucket.', err)
-      console.log('Bucket created successfully in "us-east-1".')
-    })
+  if (!exists) {
+    minioClient.makeBucket("mybucket", "us-east-1", function (err) {
+      if (err) return console.log("Error creating bucket.", err);
+      console.log('Bucket created successfully in "us-east-1".');
+    });
   }
-})
+  //  Bucket Notification //////////////////////////////////////////
+  //  Create a new notification object
+  var bucketNotification = new Minio.NotificationConfig();
+
+  // Setup a new Queue configuration
+  var arn = Minio.buildARN("minio", "sqs", "", 1, "webhook");
+  var queue = new Minio.QueueConfig(arn);
+
+  queue.addFilterSuffix(".mp4");
+  queue.addEvent(Minio.ObjectCreatedAll);
+
+  // Add the queue to the overall notification object
+  bucketNotification.add(queue);
+
+  minioClient.setBucketNotification("mybucket", bucketNotification, function (
+    err
+  ) {
+    if (err) return console.log(err);
+    console.log("Success");
+  });
+
+  return console.log("Bucket exists.");
+});
